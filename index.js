ce851f1b8e990aadad655781c9bc168720f124b5
Andy Patari <andy.patari@gmail.com>
2023-07-21 09:52:02 -0400

    passing tests

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // code your solution here
+
+function superbowlWin(arr) {
+    const res = arr.find(arr => (arr.result === "W"))
+    if (res) {
+        return res.year
+    }
+    return res
+
+}
+
+/* function winningYear(arr) {
+    return (arr.result === "W")
+}
+
+function superbowlWin(arr) {
+    return arr.find(winningYear)
+} */
+
